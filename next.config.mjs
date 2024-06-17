e496d7d76983c588b80a29903a5d843ddf9cb153
jqiufu-ubc <jqiufu@student.ubc.ca>
2024-06-17 00:44:41 -0700

    Fixed config to load images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'digital-marketplace-app-production.up.railway.app']
+    domains: ['localhost', 'digital-market-app.up.railway.app']
   }
 };
 
